3ca445cac7ecb1d5a5d56e939ee5a1ddd77a5041
GeorgiyTsoy-99 <81908281+GeorgiyTsoy-99@users.noreply.github.com>
2021-04-06 21:01:28 +0500

    adding files

---
+++ fibonacci.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstNum := 0
+	secondNum := 1
+
+	fmt.Println(firstNum)
+	fmt.Println(secondNum)
+	for i := 0; i < 10; i++ {
+		sum := firstNum + secondNum
+		fmt.Println(sum)
+		firstNum = secondNum
+		secondNum = sum
+	}
+}
