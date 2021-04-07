a0fb959d3073074d42b27a7afd24ba4d2d208605
GeorgiyTsoy-99 <81908281+GeorgiyTsoy-99@users.noreply.github.com>
2021-04-07 22:42:45 +0500

    adding files

--- FizzBuzz.go
+++ FizzBuzz.go
@@ -5,9 +5,11 @@ import "fmt"
 func fizzbuzz(i int) {
 	if i%3 == 0 && i%5 == 0 {
 		fmt.Println("FizzBuzz")
-	} else if i%5 == 0 {
+	}
+	if i%5 == 0 {
 		fmt.Println("Buzz")
-	} else if i%3 == 0 {
+	}
+	if i%3 == 0 {
 		fmt.Println("Fizz")
 	} else {
 		fmt.Println(i)
